a39321ffaffab63b9cab1448d291bb45764d77fa
Gabitho <106996555+Gabitho@users.noreply.github.com>
2025-06-08 11:41:10 +0200

    :]

--- src/main/java/com/gplugins/commands/GGiveCommand.java
+++ src/main/java/com/gplugins/commands/GGiveCommand.java
@@ -8,6 +8,8 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.components.CustomModelDataComponent;
+import java.util.Arrays;
 
 import java.util.Collections;
 import java.util.List;
